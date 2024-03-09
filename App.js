b3ea2ee05f4748f4ea282d96121582338aaa3677
Muhammad Tamim <mdt40234@gmail.com>
2024-03-10 02:24:06 +0600

    show on iu my first react native app

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>This is my first App</Text>
+      <Text>This app also can run on iphone 15</Text>
     </View>
   );
 }
